672b86f393fc8cc1c47dbea3366b8038d848a285
Dicky Julian <dicky.julian@bnisekuritas.co.id>
2023-07-19 12:47:59 +0700

    fix: 'React' is declared but its value is never read

--- src/pages/billing/component.tsx
+++ src/pages/billing/component.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Billing = () => {
     return (
         <section className='billing_container'>
--- src/pages/monitoring/component.tsx
+++ src/pages/monitoring/component.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Monitoring = () => {
     return (
         <section className='monitoring_container'>
