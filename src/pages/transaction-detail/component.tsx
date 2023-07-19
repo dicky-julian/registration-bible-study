4c2744e0bc3d4d931c4044a4d01bfa54c359415a
Dicky Julian <dicky.julian@bnisekuritas.co.id>
2023-07-19 21:13:38 +0700

    feat: provide payment direct button

--- src/pages/transaction-detail/component.tsx
+++ src/pages/transaction-detail/component.tsx
@@ -1,3 +1,5 @@
+import { Button } from "react-bootstrap"
+
 const PaymentIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor">
         <path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 4H4c-1.11 0-1.99.89-1.99 2L2 18c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V6c0-1.11-.89-2-2-2zm0 14H4v-6h16v6zm0-10H4V6h16v2z"/>
@@ -73,6 +75,8 @@ export const TransactionDetail = () => {
                     <span className='fw-bold'>Rp100.000</span>
                 </div>
             </div>
+
+            <Button className="my-4">Ke Halaman Pembayaran</Button>
         </section>
     )
 }
